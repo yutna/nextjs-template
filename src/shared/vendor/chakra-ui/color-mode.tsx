ff37cf434ee919424095da7e49feefa05a15d544
Yutthana Siphuengchai <yutthana.s@outlook.com>
2026-02-23 23:21:28 +0700

    chore: reorder imports in color-mode.tsx for better organization

--- src/shared/vendor/chakra-ui/color-mode.tsx
+++ src/shared/vendor/chakra-ui/color-mode.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import type { IconButtonProps, SpanProps } from "@chakra-ui/react";
 import { ClientOnly, IconButton, Skeleton, Span } from "@chakra-ui/react";
 import { ThemeProvider, useTheme } from "next-themes";
-import type { ThemeProviderProps } from "next-themes";
 import * as React from "react";
 import { LuMoon, LuSun } from "react-icons/lu";
+
+import type { IconButtonProps, SpanProps } from "@chakra-ui/react";
+import type { ThemeProviderProps } from "next-themes";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface ColorModeProviderProps extends ThemeProviderProps {}
