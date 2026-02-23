c853ebb873ac78ed406f0deba181314234569cdc
Yutthana Siphuengchai <yutthana.s@outlook.com>
2026-02-23 21:27:05 +0700

    chore: remove unused CSS files and update layout to use Noto Sans Thai font

---
+++ src/shared/config/fonts.ts
@@ -0,0 +1,7 @@
+import { Noto_Sans_Thai } from "next/font/google";
+
+export const notoSansThai = Noto_Sans_Thai({
+  subsets: ["thai", "latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-noto-sans-thai",
+});
