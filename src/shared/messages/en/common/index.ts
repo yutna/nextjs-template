1bb8e4fb7ea6e6fc32da110d24e34f8557cc8a61
Yutthana Siphuengchai <yutthana.s@outlook.com>
2026-02-25 10:33:57 +0700

    feat: add English and Thai localization files for common messages

---
+++ src/shared/messages/en/common/index.ts
@@ -0,0 +1,13 @@
+import actions from "./actions.json";
+import datetime from "./datetime.json";
+import pagination from "./pagination.json";
+import status from "./status.json";
+import validation from "./validation.json";
+
+export const common = {
+  actions,
+  datetime,
+  pagination,
+  status,
+  validation,
+};
